764b541703fdf09224f441125caf8a05f3d94a56
Mikey Mann <mikey.mannx3@gmail.com>
2021-08-02 11:37:13 -0700

    added POST update route

--- routes/books.js
+++ routes/books.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const Book = require('../models').Book;
 
-/* Handler function to wrap each route. */
+
+/* Handler function */
 function asyncHandler(cb){
   return async(req, res, next) => {
     try {
@@ -22,10 +23,31 @@ router.get('/', asyncHandler(async (req, res, next) => {
 }));
 
 
+
 // GET REQUEST: Display the create new book view
 router.get('/new', asyncHandler(async (req, res, next) => {
   res.render('new-book', { book: {}, title: "New Book" });
 }));
+
+
+
+// GET REQUEST: Display book detail form by primary key
+router.get('/:id', asyncHandler(async (req, res, next) => {
+  const book = await Book.findByPk(req.params.id);
+
+  if(book) {
+    await book.update(req.body);
+    res.render('update-book', {book, title: `Update Book - ${book.title}`});
+  } else {
+    const error = new Error(`Looks like the book ID ${req.params.id} doesn't exist in our records`)
+    error.status = 404;
+    throw error;
+  }
+
+}));
+
+
+
 
 // POST REQUEST: Insert newly created book into the database
 router.post('/new', asyncHandler(async (req, res, next) => {
@@ -46,21 +68,31 @@ router.post('/new', asyncHandler(async (req, res, next) => {
   }
 }));
 
+// POST REQUEST: Update book details in the database
+router.post('/:id', asyncHandler(async (req, res) => {
+  let updateBook;
 
-// GET REQUEST: Display book detail form by primary key
-router.get('/:id', asyncHandler(async (req, res, next) => {
-  const book = await Book.findByPk(req.params.id);
+  try {
+    updateBook = await Book.findByPk(req.params.id);
+    
+    if(updateBook) {
+      await updateBook.update(req.body);
+      res.redirect('/books');
+    } else {
+      throw error;
+    }
 
-  if(book) {
-    await book.update(req.body);
-    res.render('update-book', {book, title: `Update Book - ${book.title}`});
-  } else {
-    const error = new Error(`Looks like the book ID ${req.params.id} doesn't exist in our records`)
-    error.status = 404;
-    throw error;
+  } catch(error) {
+      if(error.name === 'SequelizeValidationError') {
+        console.log(req.body)
+        updateBook = await Book.build(req.body);
+        res.render('update-book', {updateBook, errors: error.errors})
+      } else {
+        throw error;
+      }
   }
-
 }));
+
 
 
 module.exports = router;
