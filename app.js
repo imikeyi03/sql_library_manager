7dac91fe40381e6a2e170b94bce631f17320b125
Mikey Mann <mikey.mannx3@gmail.com>
2021-08-02 15:00:36 -0700

    cleaned up unnecessary code

--- app.js
+++ app.js
@@ -3,15 +3,13 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const app = express();
 
 
 // Initalize main and book routes
 const routes = require('./routes/index');
 const books = require('./routes/books');
 
-
-const {sequelize} = require('./models/index'); //import Sequelize
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
